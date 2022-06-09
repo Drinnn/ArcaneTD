418997dfb6de1e08e06b2287e679f79093b94f1b
Drinnn <pedro_lontro@hotmail.com>
2022-06-09 20:34:22 -0300

    feat(building manager - wip): add building type

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -2,20 +2,33 @@ using UnityEngine;
 
 public class BuildingManager : MonoBehaviour
 {
-    [SerializeField] private Transform pfWoodHarvester;
-
     private Camera _mainCamera;
+    private BuildingTypeListSO _buildingTypeList;
+    private BuildingTypeSO _buildingType;
 
-    private void Awake()
+    private void Start()
     {
         _mainCamera = Camera.main;
+
+        _buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+        _buildingType = _buildingTypeList.list[0];
     }
 
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Instantiate(pfWoodHarvester, GetMouseWorldPosition(), Quaternion.identity);
+            Instantiate(_buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _buildingType = _buildingTypeList.list[0];
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            _buildingType = _buildingTypeList.list[1];
         }
     }
 
